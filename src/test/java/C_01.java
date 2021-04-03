ac5d53d66b0d983104647df784fe391d3bbd6931
elisontl <77907811+elisontl@users.noreply.github.com>
2021-04-03 11:11:34 +0800

    Update C_01.java

--- src/test/java/C_01.java
+++ src/test/java/C_01.java
@@ -2,17 +2,10 @@
 import org.apache.http.util.Asserts;
 
 /**
- * Test Class C_01
+ * Class Desc ：单元测试类
  */
 public class C_01 {
 
-    // 枚举、结构重构、泛型继承
-    public void test_01() {
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-        Asserts.notNull(1, "该值有问题");
-    }
+   
 
 }
